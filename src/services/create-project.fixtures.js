3851d33eccc58f8453d5285441aec61f56744928
mathieudutour <mathieu@dutour.me>
2018-10-17 09:32:41 +0200

    fix type

--- src/services/create-project.fixtures.js
+++ src/services/create-project.fixtures.js
@@ -1,6 +1,8 @@
 // @flow
+import type { ProjectInternal } from '../types';
+
 // Fake data for manually testing project-creation flow.
-export const FAKE_CRA_PROJECT = {
+export const FAKE_CRA_PROJECT: ProjectInternal = {
   name: 'haidddd',
   version: '0.1.0',
   private: true,
@@ -15,9 +17,5 @@ export const FAKE_CRA_PROJECT = {
     test: 'react-scripts test --env=jsdom',
     eject: 'react-scripts eject',
   },
-  guppy: {
-    id: 'haidddd',
-    name: 'Haidddd',
-    icon: '/static/media/icon_blueorange.174c0078.jpg',
-  },
+  __skpm_createdAt: Date.now(),
 };
